7fea46a08e6b0c5cf7f4c3c2d063f0a14133ed3c
gw298 <gw2988@naver.com>
2019-11-28 20:24:02 +0900

    2019-11-28 fragment_send and fragment_share update.

--- app/src/main/java/com/example/moneysavingapp_ver2/ui/send/SendFragment.java
+++ app/src/main/java/com/example/moneysavingapp_ver2/ui/send/SendFragment.java
@@ -23,11 +23,11 @@ public class SendFragment extends Fragment {
         sendViewModel =
                 ViewModelProviders.of(this).get(SendViewModel.class);
         View root = inflater.inflate(R.layout.fragment_send, container, false);
-        final TextView textView = root.findViewById(R.id.text_send);
+//        final TextView textView = root.findViewById(R.id.text_send);
         sendViewModel.getText().observe(this, new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
-                textView.setText(s);
+//                textView.setText(s);
             }
         });
         return root;
--- app/src/main/java/com/example/moneysavingapp_ver2/ui/share/ShareFragment.java
+++ app/src/main/java/com/example/moneysavingapp_ver2/ui/share/ShareFragment.java
@@ -23,11 +23,11 @@ public class ShareFragment extends Fragment {
         shareViewModel =
                 ViewModelProviders.of(this).get(ShareViewModel.class);
         View root = inflater.inflate(R.layout.fragment_share, container, false);
-        final TextView textView = root.findViewById(R.id.text_share);
+       // final TextView textView = root.findViewById(R.id.text_share);
         shareViewModel.getText().observe(this, new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
-                textView.setText(s);
+         //       textView.setText(s);
             }
         });
         return root;
