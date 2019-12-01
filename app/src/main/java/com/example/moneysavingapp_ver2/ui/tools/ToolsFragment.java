e8ba4ffd8813f27d9dbf719f8d48cf62849a8e10
YoonSuHyeon <96-05-03@naver.com>
2019-12-01 20:26:13 +0900

    2019-12-01 Tools 전체 채팅방 데이터 받아오기

--- app/src/main/java/com/example/moneysavingapp_ver2/ui/tools/ToolsFragment.java
+++ app/src/main/java/com/example/moneysavingapp_ver2/ui/tools/ToolsFragment.java
@@ -11,6 +11,7 @@ import android.view.Window;
 import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
@@ -27,15 +28,19 @@ import com.example.moneysavingapp_ver2.ChatRoomAdapter;
 import com.example.moneysavingapp_ver2.FragmentDialog_Room_Create;
 import com.example.moneysavingapp_ver2.Message;
 import com.example.moneysavingapp_ver2.R;
+import com.example.moneysavingapp_ver2.Sign_UpActivity;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 
 public class ToolsFragment extends Fragment {
     private  String uid,nickname;
     private ArrayList<ChatRoom> roomlist;
-    private ToolsViewModel toolsViewModel;
+
     private RecyclerView recyclerView;
     private ChatRoomAdapter cr_Adapter;
     private Button bt_roomcreat;
@@ -53,22 +58,20 @@ public class ToolsFragment extends Fragment {
 
 
     }*/
-
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        roomlist= new ArrayList<ChatRoom>();
-        database= FirebaseDatabase.getInstance().getReference();
 
-        roomlist.add(new ChatRoom("방"));
-        roomlist.add(new ChatRoom("방2"));
+        database= FirebaseDatabase.getInstance().getReference();
+        roomlist= new ArrayList<ChatRoom>();
+
+
 
         Bundle bundle = getArguments();
         uid=bundle.getString("uid");
         nickname=bundle.getString("nickname");
-        Log.d("assss",uid+":"+nickname);
+        Log.d("assss",roomlist.toString());
 
-        toolsViewModel =
-                ViewModelProviders.of(this).get(ToolsViewModel.class);
+
         final View root = inflater.inflate(R.layout.fragment_tools, container, false);
         bt_roomcreat = (Button) root.findViewById(R.id.bt_roomcreat);
 
@@ -84,6 +87,18 @@ public class ToolsFragment extends Fragment {
 
                     @Override
                     public void finish(String result,String result1) {
+                        //테스트
+                        result="서울";
+                        result1="방이름";
+                        if(result!=null&& result1!=null){ //카테고리 ,방이름이 null이 아닐 경우  방을 생성해준다.
+                            database =database.child("Chats").child("서울").push();
+                            database.child("Name").setValue(result1);
+                            database.child("Member").setValue(nickname);
+                            Message message = new Message("방이 개설되었습니다.",uid);
+                            database.child("Conversation").push().setValue(message);
+                            database =FirebaseDatabase.getInstance().getReference();
+                            database.child("Users").child(uid).child("myroom").push().setValue(result1);
+                        }
                         Log.d("아아아",result);
                         Log.d("이이이이",result1);
                         // result에 dialog에서 보낸값이 저장되어 돌아옵니다.
@@ -96,7 +111,7 @@ public class ToolsFragment extends Fragment {
 
             }
         });
-       // final TextView textView = root.findViewById(R.id.text_tools);
+
 
 
 
@@ -104,22 +119,48 @@ public class ToolsFragment extends Fragment {
         recyclerView=root.findViewById(R.id.reclv_mychat);
 
 
-        cr_Adapter =new ChatRoomAdapter(roomlist);
 
-        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getActivity());
-        recyclerView.setLayoutManager(mLayoutManager);
-        recyclerView.setItemAnimator(new DefaultItemAnimator());
-        recyclerView.setAdapter(cr_Adapter);
-        toolsViewModel.getText().observe(this, new Observer<String>() {
+
+        database = FirebaseDatabase.getInstance().getReference();
+        database = database.child("Chats");
+        ValueEventListener vListener = new ValueEventListener() {
             @Override
-            public void onChanged(@Nullable String s) {
-       //         textView.setText(s);
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for (DataSnapshot noteDataSnapshot : dataSnapshot.getChildren()) {
+                    for (DataSnapshot da : dataSnapshot.getChildren()) {
+                        for(DataSnapshot dd :da.getChildren()){
+                            for(DataSnapshot dx : dd.getChildren()){
+                                ChatRoom chatRoom = new ChatRoom(dx.getValue().toString());
+                                if(dx.getKey().contains("Name")) {
+                                    roomlist.add(chatRoom);
+                                    Log.d("ss", dx.toString());
+                                }
+                            }
+                        }
+                    }
+                }
+                cr_Adapter =new ChatRoomAdapter(roomlist);
+
+                RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getActivity());
+                recyclerView.setLayoutManager(mLayoutManager);
+                recyclerView.setItemAnimator(new DefaultItemAnimator());
+                recyclerView.setAdapter(cr_Adapter);
             }
-        });
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+
+        };
+        database.addListenerForSingleValueEvent(vListener);
+
 
 
 
 
         return root;
     }
+
+
 }
